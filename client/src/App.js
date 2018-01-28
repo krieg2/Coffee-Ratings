7b994512b3522215ceebc2803f97c0c53a6b7dca
krieg2 <aaronk1@yahoo.com>
2018-01-27 21:48:15 -0500

    added login page and router

--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-  <div>
-    <nav class="navbar navbar-default">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="#">COFFEE RATING APP</a>
+      <Router>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
         </div>
-        <ul class="nav navbar-nav">
-          <li class="active"><a href="#">Home</a></li>
-          <li><a href="#">  Coffee Reviews </a></li>
-          <li><a href="#">| Shop Reviews                 </a></li>
-          <li><a href="#">Sign Up</a></li>
-          <li><a href="#"> Sign In</a></li>
-        </ul>
-      </div>
-    </nav>
-
-    <div class="jumbotron text-center" style={{backgroundColor: "#74533f"}}>
-      <h1 style={{color: "#ebd69c", textTransform: "uppercase"}}>Coffee rating app</h1>
-      <p style={{color: "#ebd69c"}}><em>Enhance Your Coffee Experience!</em></p> 
-    </div>
-  
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-4" style={{backgroundColor: "#94b6d2"}}>
-          <h3>Column 1</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-          <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-        </div>
-        <div class="col-sm-4" style={{backgroundColor: "#94b6d2"}}>
-          <h3>Column 2</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-          <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-        </div>
-        <div class="col-sm-4" style={{backgroundColor: "#94b6d2"}}>
-          <h3>Column 3</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-          <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-        </div>
-      </div>
-
-      <footer className="page-footer blue center-on-small-only">
-        <div className="footer-copyright">
-            <div className="container-fluid" style={{color: "#ebd69c"}}>
-                © 2018 Copyright COFFEE RATING APP
-            </div>
-        </div>
-      </footer>  
-    </div>
-  </div>);
+      </Router>
+    );
   }
 }
 
---
+++ client/src/pages/Home.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Home extends Component {
+
+  render() {
+
+    return (
+      <div>
+        <nav class="navbar navbar-default">
+          <div class="container-fluid">
+            <div class="navbar-header">
+              <a class="navbar-brand" href="#">COFFEE RATING APP</a>
+            </div>
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#">  Coffee Reviews </a></li>
+              <li><a href="#">| Shop Reviews                 </a></li>
+              <li><a href="#">Sign Up</a></li>
+              <li><Link to="/login"> Log In</Link></li>
+            </ul>
+          </div>
+        </nav>
+
+        <div class="jumbotron text-center" style={{backgroundColor: "#74533f"}}>
+          <h1 style={{color: "#ebd69c", textTransform: "uppercase"}}>Coffee rating app</h1>
+          <p style={{color: "#ebd69c"}}><em>Enhance Your Coffee Experience!</em></p> 
+        </div>
+      
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-4" style={{backgroundColor: "#94b6d2"}}>
+              <h3>Column 1</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+              <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+            </div>
+            <div class="col-sm-4" style={{backgroundColor: "#94b6d2"}}>
+              <h3>Column 2</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+              <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+            </div>
+            <div class="col-sm-4" style={{backgroundColor: "#94b6d2"}}>
+              <h3>Column 3</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+              <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+            </div>
+          </div>
+
+          <footer className="page-footer blue center-on-small-only">
+            <div className="footer-copyright">
+                <div className="container-fluid" style={{color: "#ebd69c"}}>
+                    © 2018 Copyright COFFEE RATING APP
+                </div>
+            </div>
+          </footer>  
+        </div>
+      </div>);
+  }
+}
+
+export default Home;
