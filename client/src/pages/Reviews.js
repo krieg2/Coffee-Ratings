a3485ecf92406529171656aa5af34365b686ea55
krieg2 <aaronk1@yahoo.com>
2018-01-30 20:46:05 -0500

    starred ratings control

--- client/src/pages/Reviews.js
+++ client/src/pages/Reviews.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Grid, Col, Row, Panel,
+import { Link } from 'react-router-dom';
+import { Grid, Col, Row, Panel, FormControl,
          FormGroup, Checkbox, ControlLabel } from 'react-bootstrap';
 import API from '../utils/API';
 
@@ -26,35 +27,37 @@ class Login extends Component {
     return(
       <Grid fluid>
         <Row>
-          <Col xs={3} sm={3} md={2}>
+          <Col xs={12} sm={3} md={3}>
             <Panel>
-		      <Panel.Heading style={{backgroundColor: "#dd8047"}}>Search filters</Panel.Heading>
-		      <Panel.Body style={{padding: "20px"}}>
-			    <form>
-			    <FormGroup>
-			      <ControlLabel>Roast</ControlLabel>
-			      <Checkbox>Dark</Checkbox>
-			      <Checkbox>Medium</Checkbox>
-		          <Checkbox>Light</Checkbox>
-			    </FormGroup>
-			    <FormGroup>
-			      <ControlLabel>Region</ControlLabel>
-			      <Checkbox>Central America</Checkbox>
-			      <Checkbox>South America</Checkbox>
-		          <Checkbox>Africa</Checkbox>
-		          <Checkbox>Indonesia</Checkbox>
-		          <Checkbox>Inda</Checkbox>
-		          <Checkbox>Jamaica</Checkbox>
-		          <Checkbox>Hawaii</Checkbox>
-			    </FormGroup>
-			    </form>
-		      </Panel.Body>
-		    </Panel>
+    		      <Panel.Heading style={{backgroundColor: "#dd8047"}}>Search filters</Panel.Heading>
+    		      <Panel.Body style={{padding: "20px"}}>
+    			    <form>
+    			    <FormGroup>
+    			      <ControlLabel>Roast</ControlLabel>
+    			      <Checkbox>Dark</Checkbox>
+    			      <Checkbox>Medium</Checkbox>
+    		          <Checkbox>Light</Checkbox>
+    			    </FormGroup>
+    			    <FormGroup>
+    			      <ControlLabel>Region</ControlLabel>
+    			      <Checkbox>Central America</Checkbox>
+    			      <Checkbox>South America</Checkbox>
+    		          <Checkbox>Africa</Checkbox>
+    		          <Checkbox>Indonesia</Checkbox>
+    		          <Checkbox>Inda</Checkbox>
+    		          <Checkbox>Jamaica</Checkbox>
+    		          <Checkbox>Hawaii</Checkbox>
+    			    </FormGroup>
+    			    </form>
+    		      </Panel.Body>
+    		    </Panel>
           </Col>
-          <Col xs={8} sm={6} md={5}>
+          <Col xs={12} sm={9} md={9}>
             <Panel>
               <Panel.Heading style={{backgroundColor: "#dd8047"}}>Coffee 1</Panel.Heading>
-              <Panel.Body style={{padding: "40px"}}>
+              <Panel.Body style={{padding: "10px"}}>
+
+                <Link to="/rate">Rate It</Link>
               </Panel.Body>
             </Panel>
           </Col>
