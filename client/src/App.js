0fb6fcc45d5e57a2eacbb55e449786404299eef4
krieg2 <aaronk1@yahoo.com>
2018-01-30 22:56:55 -0500

    protect the rating route

--- client/src/App.js
+++ client/src/App.js
@@ -68,7 +68,7 @@ class App extends Component {
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={Signup} />
           <Route exact path="/reviews" component={Reviews} />
-          <Route exact path="/rate" component={Rate} />
+          <PrivateRoute exact path="/rate" component={Rate} />
         </div>
       </Router>
     );
