d22446cd914fe29066c550e388197a5f638cbf89
krieg2 <aaronk1@yahoo.com>
2018-03-21 21:10:35 -0400

    change carousel to display small or laarge images

--- client/src/components/TopTenCoffees.js
+++ client/src/components/TopTenCoffees.js
@@ -21,21 +21,30 @@ class TopTenCoffees extends React.Component {
 
   render() {
 
-    const imageStyle = {
-      height: "485px",
-      margin: "0 auto",
-      padding: "0",
-      borderRadius: "5px",
-      border: "2px solid #dd8047"
-    };
+        const imageStyle = {
+          height: "485px",
+          lineHeight: "485px",
+          width: "550px",
+          margin: "0 auto",
+          padding: "0",
+          borderRadius: "5px",
+          border: "2px solid #337ab7", //#dd8047
+          background: "#fff",
+          textAlign: "center",
+          overflow: "hidden"
+        };
 
   	return (<div>
       <h1 style={{marginLeft: "20px", color: "#74533f"}}>Top Ten Coffees:</h1>
       <Carousel>
   		{this.state.results.map( (item, index) => {
+
+
   		  return(<Carousel.Item key={index}>
-                 <Image src={item.image} alt={item.title} style={imageStyle}/>
-                 <Carousel.Caption>
+                 <div style={imageStyle}>
+                   <Image src={item.image} alt={item.title} style={{minHeight: "160px", maxHeight: "485px", verticalAlign: "middle"}} />
+                 </div>
+                 <Carousel.Caption style={{color: "#74533f", textShadow: "2px 1px 2px white"}}>
                    <h1>{`#${index+1}`}</h1>
                    <h3>{item.brand}</h3>
                    <p>{item.title}</p>
